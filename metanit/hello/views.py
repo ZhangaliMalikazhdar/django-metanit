1a037d44a4d149faabcfe0eb0341ebb3d6a475af
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-07 14:12:37 +0600

    header in network

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -3,15 +3,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    host = request.META['HTTP_HOST']
-    user_agent = request.META['HTTP_USER_AGENT']
-    path = request.path
-
-    return HttpResponse(f'''
-        <p>Host: {host}</p>
-        <p>Path: {path}</p>
-        <p>User-agent: {user_agent}</p>
-    ''')
+    return HttpResponse('haol', headers={"Secret": "o2i3q4u02"})
 
 
 def about(request, name, age):
