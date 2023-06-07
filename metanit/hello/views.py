6d1ae362104ba2103e3defba66457b244e11490b
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-07 13:45:25 +0600

    requests

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -3,4 +3,24 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hola metanit')
+    host = request.META['HTTP_HOST']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path = request.path
+
+    return HttpResponse(f'''
+        <p>Host: {host}</p>
+        <p>Path: {path}</p>
+        <p>User-agent: {user_agent}</p>
+    ''')
+
+
+def about(request, name, age):
+    return HttpResponse(f'''
+        <h2>about user</h2>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+    ''')
+
+
+def contact(request):
+    return HttpResponse('contact')
