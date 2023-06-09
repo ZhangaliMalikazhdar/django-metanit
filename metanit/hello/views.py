582a0dc8125c94533b30958fed2fd5a6f91b86b0
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-09 14:27:09 +0600

    obj by class

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -3,11 +3,27 @@ from django.http import HttpResponse, HttpResponseNotFound, \
     HttpResponseForbidden, HttpResponseBadRequest, \
     HttpResponseRedirect, HttpResponsePermanentRedirect, \
     JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
 
 
 def index(request):
-    return JsonResponse({'name': 'Tom', 'age': 38})
+    bob = Person('Bob', 32)
+    return JsonResponse(bob, safe=False, encoder=PersonEncoder)
 
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+class PersonEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, Person):
+            return {'name': o.name, 'age': o.age}
+            # return o.__dict__
+        return super().default(o)
+    
 
 # def access(request, age):
 #     if age not in range(1, 111):
