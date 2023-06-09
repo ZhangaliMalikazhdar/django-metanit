5f0977531e3b04ec3f806df5f5dfc53bbe632044
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-10 02:18:01 +0600

    include with arguments

--- metanit/metanit/urls.py
+++ metanit/metanit/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html', extra_context={'site': 'mota'})),
     path('contacts/', TemplateView.as_view(template_name='contact.html')),
 ]
