d5892e29d6485e5be9abad2fe838723709627b23
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-11 20:12:05 +0600

    error field in form in simple mode

--- metanit/hello/forms.py
+++ metanit/hello/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField(min_length=3)
     age = forms.IntegerField(min_value=1, max_value=100)
-
+    required_css_class = 'field'
+    error_css_class = 'error'
