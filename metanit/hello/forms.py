5ef5cfc82ded2f0ccd696e841cd24002646d4f40
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-11 20:08:19 +0600

    error in form with color

--- metanit/hello/forms.py
+++ metanit/hello/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
+    name = forms.CharField(min_length=3)
+    age = forms.IntegerField(min_value=1, max_value=100)
+
