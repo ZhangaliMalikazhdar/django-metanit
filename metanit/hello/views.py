9513f49997f7c03ec7898c90604bc2abe208c5cc
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-13 01:52:14 +0600

    orderby values(list) aggregate last first exists union inter diff

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,29 +1,39 @@
-from .models import Order, Person
-from datetime import datetime, date, time
+from .models import Person
+from django.db.models import Avg, Min, Max, Sum
 
 
-if Order.objects.count() == 0:
-    Order.objects.create(datetime=datetime(2021, 12, 26, 11, 25, 34))
-    Order.objects.create(datetime=datetime(2022, 5, 12, 12, 25, 34))
-    Order.objects.create(datetime=datetime(2022, 5, 22, 13, 25, 34))
-    Order.objects.create(datetime=datetime(2022, 8, 19, 14, 25, 34))
-
-orders = Order.objects.filter(datetime__month=5)
-for order in orders:
-    print(order)
+people = Person.objects.order_by('-name', 'age')
+for person in people:
+    print(person)
 print()
-orders = Order.objects.filter(datetime__month__gt=5)
-for order in orders:
-    print(order)
-print()
-orders = Order.objects.filter(datetime__date=date(2022, 5, 22))
-for order in orders:
-    print(order)
-print()
-orders = Order.objects.filter(datetime__time__gt=time(12, 20, 0))
-for order in orders:
-    print(order)
 
-print(Person.objects.filter(name="Bob") & Person.objects.filter(age__range=(1, 50)))
-print(Person.objects.filter(name="Tom") | Person.objects.filter(age=10))
-print(Person.objects.filter(name="Tom") ^ Person.objects.filter(age=22))
+# people = Person.objects.values('name')
+# people = Person.objects.values_list()
+# people = Person.objects.values_list('name', flat=True)
+people = Person.objects.values_list('name', flat=True).distinct()
+print(people)
+print()
+
+bob = Person.objects.filter(name='Bob')
+akame = Person.objects.filter(name='Akame')
+print(akame.union(bob, all=False).values())
+# union intersection difference
+print()
+
+print(Person.objects.latest('id').id)
+print(Person.objects.earliest('-id').id)
+
+print(Person.objects.first())
+print(Person.objects.last())
+print()
+
+print(Person.objects.filter(age__gt=134).exists())
+print(Person.objects.filter(age__gte=10).contains(Person.objects.last()))
+print()
+
+print(Person.objects.acount())
+# Что использовать: count или len? Если объекты уже ранее были загружены (например, с помощью метода all()), то оптимальнее использовать функцию len(), которая не выполняет к базе данных запрос SQL, а работает с уже загруженным набором объектов. Если же объекты ранее НЕ были загружены, то лучше выполнить метод count(), для которого не потребуется загружать все объекты из бд.
+print(Person.objects.aggregate(Avg('age')))
+print(Person.objects.aggregate(Sum('age')))
+print(Person.objects.aggregate(Min('age')))
+print(Person.objects.aggregate(Max('age')))
