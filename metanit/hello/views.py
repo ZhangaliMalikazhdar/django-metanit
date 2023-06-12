8b5842799a0296e5030925d122aeb639765e7625
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-06-12 16:24:18 +0600

    queries

--- metanit/hello/views.py
+++ metanit/hello/views.py
@@ -1,9 +1,26 @@
 from .models import Person
-import asyncio
+from django.db.models import F
 
 
-async def get_person():
-    person = await Person.objects.aget(id=1)
-    print(person.name)
+# number = Person.objects.filter(id=1).update(name='Mike')
+# Person.objects.all().update(age=F("age") + 1)
 
-asyncio.run(get_person())
+# values_for_update = {'name': 'Bob', 'age': 31}
+# bob, created = Person.objects.update_or_create(id=2, defaults=values_for_update)
+
+# f = Person.objects.get(id=1)
+# f.name = 'Zora'
+# s = Person.objects.get(id=2)
+# s.age = 10
+# n = Person.objects.bulk_update([f, s], ['name', 'age'])
+
+# Person.objects.all().delete()
+
+# for i in Person.objects.exclude(name='Bob'):
+#     print(i.name, i.age)
+
+
+print(Person.objects.filter(name__iexact='Bob'))
+print(Person.objects.filter(name__contains='k'))
+print(Person.objects.filter(name__icontains='T'))
+print(Person.objects.filter(name__regex=r'(ob|te)$'))
